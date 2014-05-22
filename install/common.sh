8030fd78bec6cdd37a934d4f2a6adaa4121e5340
juliendsv <julien.dasilva@gmail.com>
2014-05-23 00:40:19 +0100

    remove print

--- install/common.sh
+++ install/common.sh
@@ -4,7 +4,6 @@
 function test_image() {
 	version=$1
 	image=cassandra:$version
-	print $image
 	docker history $image > /dev/null 2> /dev/null
 	if [[ $? != 0 ]]; then
 		echo "Could not find Docker image $image, use 'make image VERSION=$version' to build it"
